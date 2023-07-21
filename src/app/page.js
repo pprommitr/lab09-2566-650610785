a6eaf7c74dc709220ea727e71e9651b913cefadc
pinnaree <pprommitr@gmail.com>
2023-07-22 01:44:57 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -60,7 +60,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2023" fullName="Pinnaree Prommitr" studentId="650610785" />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -9,6 +9,10 @@ export const Task = ({
     deleteTaskFunc(id);
   };
 
+  const doneBtnOnClick = () => {
+    toggleDoneTaskFunc(id);
+  };
+
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
       {/*
@@ -17,8 +21,12 @@ export const Task = ({
         But if task is not completed : 
         <span>{title}</span>
       */}
-      <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      <span className={completed ? "text-decoration-line-through" : ""}>
+        {title}
+      </span>
+      <button className="btn btn-success" onClick={doneBtnOnClick}>
+        Done
+      </button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
